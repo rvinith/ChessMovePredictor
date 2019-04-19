e530489e41a2bbb17b3348bd9d71dcfe48f9f8d5
Vinith <vinithr2@illinois.edu>
2019-04-18 22:12:29 -0500

    started prototyping class and CNN

--- project.py
+++ project.py
@@ -4,6 +4,29 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import numpy as np
+import pickle
+
+class PredictMove:
+    def __init__(self, pool_size = 2)
+class CNN(nn.Module):
+    def __init__(self):
+        super(DigitsConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(2, 16, kernel_size = 3, padding = 1)
+        self.pool = nn.MaxPool2d
+        self.conv2 = nn.Conv2d(16, 16, kernel_size = 3, padding = 1)
+        self.conv3 = nn.Conv2d(16, 16, kernel_size = 3, padding = 1)
+        self.lin1 = nn.Linear(16, 1)
+
+    # def set_parameters(self):
+
+    def forward(self, inputs):
+        # inputs = inputs.view(-1, )
+        x = x.relu(self.conv1(inputs))
+        x = x.relu(self.conv2(inputs))
+        x = x.relu(self.conv3(inputs))
+
+        return x
+
 
 def load_puzzle(pgn_handle):
     """
@@ -29,6 +52,11 @@ def fit():
     """
     with open('tactics.pgn') as pgn_handle:
         b, m = load_puzzle(pgn_handle)
+        b, m = load_puzzle(pgn_handle)
+        b, m = load_puzzle(pgn_handle)
+        # b = torch.Tensor(b)
+        print(b)
+        print(m)
         while b is not None:
             b, m = load_puzzle(pgn_handle)
 
@@ -38,3 +66,14 @@ def move(board):
     @return mv: a chess.Move
     """
     #TODO: prediction here
+
+    pass
+
+# filenames = ['tactics.pgn']
+# trainFile = open(filenames[0], 'r')
+# board, move = load_puzzle(trainFile)
+# # board = torch.tensor(board) 
+# print(board)
+# print(move)
+
+fit()
